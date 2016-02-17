d24de8d446f5f20c301e2d4eddc200fdc4420377
semicolo <semicolo@free.fr>
2016-02-17 10:28:27 -0500

    First version
    This is the first version of the script with some example files.

---
+++ ManualPlace.pl
@@ -0,0 +1,115 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+use File::Path qw(remove_tree);
+use Image::Magick;
+
+# usage : Manualplace.pl image componentsCoordinates (image resolution)
+# the componentsCoordinates file should have 6 columns, the separator is space
+# the columns order is : component name, x, y, orientation, value and package
+# lines with less or more data are ignored
+
+my $ppp = 300;
+my $markerSize = 1;
+my $file = $ARGV[0] or die "Need to get CSV file on the command line\n";
+my $file2 = $ARGV[1] or die "Need to get base image on the command line\n";
+if ($#ARGV >= 3) {
+	$ppp=$ARGV[2];
+}
+ 
+open(my $data, '<', $file) or die "Could not open '$file' $!\n";
+
+my @lines = ();
+ 
+while (my $line = <$data>) {
+  chomp $line;
+ 
+  my @fields = split " " , $line;
+  if ($#fields != 5) {
+    if ($#fields > 0) {
+	print "Invalid number of fields($#fields) on line: @fields\n";
+    }
+  } else {
+    push (@lines, \@fields);
+  }
+}
+
+my $success = mkdir("Assembly");
+if ($success == 0)
+{
+	print "The Assembly folder will be cleared, do you want to continue(y/n)?\n";
+	my $input = <STDIN>;
+	chomp($input);
+	if ($input eq "y" || $input eq "Y")
+	{
+		remove_tree("Assembly");
+		mkdir("Assembly");
+	} else {
+	print("Folder already exists\n");
+	exit 1;
+	}
+}
+
+
+my $baseImage = Image::Magick->new;
+$baseImage->Read($ARGV[1]);
+
+my $height = $baseImage->GetAttribute('height');
+
+my $index = 0;
+
+# remaining lines
+my @remaining;
+my @kept;
+
+print "Generating global image\n";
+my $image = $baseImage->Clone();
+foreach my $row (@lines) {
+	my $x = $ppp*@$row[1]/25.4;
+	my $x2 = $x+$markerSize*$ppp/25.4;
+	my $y = $height - $ppp*@$row[2]/25.4;
+	$image->Draw(fill=>"#0000FF80", primitive=>'circle', points=>"$x,$y $x2,$y");
+}
+$image->Write(filename=>"Assembly/Global.png");
+
+while ($#lines >= 0) {
+
+
+@remaining = ();
+@kept = ();
+
+my $value = $lines[0]->[4];
+my $package = $lines[0]->[5];
+print "Generating image for value:$value and package:$package\n";
+
+my $image = $baseImage->Clone();
+
+foreach my $row (@lines) {
+	if ($value eq @$row[4] and $package eq @$row[5]) {
+		push (@kept, $row);
+	} else {
+		push (@remaining, $row);
+	}
+}
+
+$image = $baseImage->Clone();
+my $name ="";
+my $quantity = 0;
+foreach my $row (@kept) {
+	$quantity++;
+	my $x = $ppp*@$row[1]/25.4;
+	my $x2 = $x+$markerSize*$ppp/25.4;
+	my $y = $height - $ppp*@$row[2]/25.4;
+	$name = join " ", $name, @$row[0];
+	$image->Draw(fill=>"#0000FF80", primitive=>'circle', points=>"$x,$y $x2,$y");
+}
+$image->Splice(height=>50, width=>0);
+$image->Annotate(pointsize=>32, text=>" ${value} x$quantity,$name", x=>0, y=>34, stretch=>'Condensed');
+$image->Write(filename=>"Assembly/Image${index}.png");
+$index += 1;
+
+@lines = (); # copy the remaining data back into the lines array
+foreach my $row (@remaining) {
+	push (@lines, $row);
+}
+}
--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # SMD-Manual-Place-Image-Generator
-This started as a Perl script to generate images to help manual pick and place of SMD components
+This started as a Perl script to generate images to help manual pick and place of SMD components.
+It needs the ImageMagick perl module, it works on my Ubuntu 14.04 server but I couldn't make the ImageMagick module work on my windows 10 PC (tried with 32 and 64 bits versions of strawberry-perl 5.20.3.2 and ImageMagick 6.9.3-3)
+
+When I build a small amount of electronic boards that have SMD components, I find useful to have images on my tablet showing me the name, value and position of the components.
+
+This script automatically generates the images using an image of the board and a CSV file of the components names, positions, values and packages.
+
+### Here's how to use it with a board designed in Cadsoft Eagle:
+- Export an image of the board
+  - On the schematic run the Electrical Rule Check and add missing values to the components that don't have one
+  - Ensure the lower left corner of your board has 0,0 coordinates, else move it
+  - Select the layers you want to show (I use 17, 18, 20, 21, 25, 31, no grid)
+  - Export the image (The script assumes 300ppp this can be changed in the first lines of the script)
+  - Crop the image to the board dimensions if needed (Eagle tends to add space around the board, get rid of that or the placement will be off)
+- Export the components positions (Run the mountsmd ULP, we want the file with the MNT extension)
+- Run the script: ManualPlace.pl Power.mnt PowerC.png
+
+The script will create an Assembly folder (it'll ask before clearing it if it already exists) and generate the images in it.
+
+#### This is my very first Perl script, remarks, ideas and improvements very welcome.
